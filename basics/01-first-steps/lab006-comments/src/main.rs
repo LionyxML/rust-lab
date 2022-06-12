fa7529163946fe80bb978022902a7675455c69ce
LionyxML <rahul.juliato@gmail.com>
2022-06-11 21:12:05 -0300

    feat: comments

---
+++ basics/01-first-steps/lab006-comments/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // Hello, there!
+
+    // Some multiline comment here!
+    // 	Do you believe me?
+    // 	No?
+    // 	Yes?
+    // 	What?
+
+    let lucky_number = 7; // This is so lucky!
+
+    // This is a lot more common way
+    let not_locky_number = 42;
+}
